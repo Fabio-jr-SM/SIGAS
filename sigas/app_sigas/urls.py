14c69ef1e990f9aaab05127bd1f9917f06f94739
Fabio Junior <fabbhio007@gmail.com>
2024-03-10 11:16:45 -0400

    update auth

--- sigas/app_sigas/urls.py
+++ sigas/app_sigas/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app_sigas import views
 urlpatterns = [
     path('cadastro/',views.cadastro,name='cadastro'),
-    path('login/',views.login,name='login')
+    path('login/',views.login,name='login'),
+    path('plataforma/',views.plataforma,name='plataforma'),
 ]
--- sigas/app_sigas/views.py
+++ sigas/app_sigas/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 def cadastro(request):
     if request.method == 'GET':
@@ -10,11 +13,33 @@ def cadastro(request):
         email = request.POST.get('email')
         senha = request.POST.get('password')
 
-        user = User.objects.get(username=username)
+        user = User.objects.filter(username=username).first()
 
         if user:
             return HttpResponse('Usuario já cadastrado')
         
-        return HttpResponse(username)
+        user = User.objects.create_user(username=username,email=email,password=senha)
+        user.save()
+        
+        return HttpResponse("Usuario cadastrado com sucesso")
+    
+
 def login(request):
-    return render(request,'login.html')
+    if request.method == "GET":
+        return render(request,'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('password')
+        
+        user = authenticate(username=username,password=senha)
+
+        if user:
+            login_django(request,user)
+
+            return HttpResponse('Autenticado')
+        else:
+            return HttpResponse('Não autenticado')
+
+@login_required(login_url="/auth/login/")    
+def plataforma(request):
+    return HttpResponse("plataforma")
