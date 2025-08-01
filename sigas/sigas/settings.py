6a754ff574ecfe49613d73ce19ed1996dbaf4e3e
Fabio Junior <fabbhio007@gmail.com>
2025-07-31 21:32:26 -0400

    update settings.py and create requirements.txt

--- sigas/sigas/settings.py
+++ sigas/sigas/settings.py
@@ -139,10 +139,3 @@ ROLEPERMISSIONS_REDIRECT_TO_LOGIN = True
 #Email
 
 DEFAULT_FROM_EMAIL = "sistema.sigas@gmail.com"
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST = config('EMAIL_HOST')
